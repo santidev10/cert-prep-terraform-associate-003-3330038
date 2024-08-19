76d60c958cc3709c727a53482efc693d3ad285e2
Santiago <santiago_gallo@hotmail.com>
2024-08-19 17:14:57 -0500

    update 06_02

--- providers.tf
+++ providers.tf
@@ -2,20 +2,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = ""
-  # Configuration options
+  region                   = "us-east-1"
+  profile                  = "santi"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
 
 provider "aws" {
-  alias = "west"
-  region = "us-west-1"
-  profile = ""
+  alias                    = "west"
+  region                   = "us-west-1"
+  profile                  = "santi"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
